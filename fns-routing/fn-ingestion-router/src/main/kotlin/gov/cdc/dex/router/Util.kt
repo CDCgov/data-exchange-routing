86760029ebd4af37acfb7fa9d8bcef4afa60227e
Hristov <eph0@cdc.gov>
2023-10-31 12:09:53 -0400

    rewrite, externalize configs

--- fns-routing/fn-ingestion-router/src/main/kotlin/gov/cdc/dex/router/Util.kt
+++ fns-routing/fn-ingestion-router/src/main/kotlin/gov/cdc/dex/router/Util.kt
@@ -1,3 +1,19 @@
 package gov.cdc.dex.router
 
+import com.google.gson.annotations.SerializedName
+import java.net.URI
+
 val resourceAsText: String.() -> String? = { object {}.javaClass.getResource(this)?.readText()}
+
+fun parseBlobURI( url:String): BlobURI {
+    val uri = URI(url)
+    val host = uri.host
+    val path = uri.path.substringAfter("/")
+    val containerName = path.substringBefore("/")
+    return BlobURI(
+            host,
+            storageAccount = host.substringBefore("blob.core.windows.net"),
+            containerName = path.substringBefore("/"),
+            fileName = path.substringAfter(containerName+"/")
+    )
+}
